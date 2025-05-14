d965febb558ae098026928bf656654a905a341b8
EvgenMimicLawyer <foxmossaur@gmail.com>
2025-05-14 20:01:16 +0300

    Delete library directory

--- library/AuthorRepository.java
+++
@@ -1,6 +0,0 @@
-package com.university.library;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorRepository extends JpaRepository<Author, Long> {
-} 
--- library/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.university.library;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-    List<Book> findByAuthorId(Long authorId);
-} 
